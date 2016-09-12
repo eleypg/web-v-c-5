fe95028d25ae769ab95099e44ed304fa6c023944
eleypg <elisa@MacBook-Pro-de-Elisa-2.local>
2016-09-12 11:17:57 -0500

    actualización de ejercicio uno en carpeta y subida de todo el ejercicio dos

---
+++ ejercicios/ejercicio_tres_js/src/index.js
@@ -0,0 +1,21 @@
+/*var AActual = 2016;
+var ANacimiento = 1995;
+var Edad = AActual - ANacimiento;
+
+console.log(Edad);*/
+
+/*var radio = 5;
+var pi = 3.1416;
+
+var peri = pi * (radio*2);
+var area = pi * (radio*radio);
+
+console.log('El perímetro del círculo es: ' + peri);
+console.log('El area del círculo es: ' + area);*/
+
+var faren = 90;
+var cel = ((faren-32)*5) / 9;
+
+
+console.log(cel);
+
